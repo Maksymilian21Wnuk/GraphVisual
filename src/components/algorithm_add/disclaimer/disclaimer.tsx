8a97a63de38f77f7fd524fc433f5feda21cfb930
meks <maksymilian21wnuk@gmail.com>
2024-12-29 12:49:23 +0100

    disclaimer

--- src/components/algorithm_add/disclaimer/disclaimer.tsx
+++ src/components/algorithm_add/disclaimer/disclaimer.tsx
@@ -6,8 +6,7 @@ export default function Disclaimer() {
     return (
         <div>
             <h1 className="h1-custom text-center bg-gray-100 m-5 rounded-md">
-            Disclaimer: this page should be used for development purpose and thus will not be interesting 
-            for users focused on learning.
+            Disclaimer: this page should be used for development purpose
             </h1>
         </div>
     )
